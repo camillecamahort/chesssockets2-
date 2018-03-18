6111cd08d590e1b3f5d21bacc0a85835ca13385e
Miria <soda.mia@gmail.com>
2018-03-18 14:44:12 -0700

    transfering files

---
+++ default.js
@@ -0,0 +1,32 @@
+const port = process.env.PORT || 10001;
+const server = require("http").Server();
+var io = require("socket.io")(server);
+
+var board;
+var game;
+var socket = io();
+
+window.onload = function () {initGame();};
+
+var initGame = function() {
+   var cfg = {
+       draggable: true,
+       position: 'start',
+       onDrop: handleMove,
+   };
+
+   board = new ChessBoard('gameBoard', cfg);
+   game = new Chess();
+};
+
+var handleMove = function(source, target ) {
+    var move = game.move({from: source, to: target});
+
+    if (move === null)  return 'snapback';
+    else socket.emit('move', move);
+};
+
+socket.on('move', function(msg) {
+    game.move(msg);
+    board.position(game.fen());
+});
